26e9107fe9c34b191fe417f0da571ad27a7cd484
Rifaldi01 <rifaldipratama2004@gmail.com>
2024-12-13 10:09:05 +0700

    acce&item

--- app/Http/Controllers/manager/DashboardController.php
+++ app/Http/Controllers/manager/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\manager;
 
 use App\Http\Controllers\Controller;
+use App\Models\Item;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,10 +11,18 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $itemsByCategory = Item::with('cat')
+            ->select('itemcategory_id',
+                \DB::raw('count(*) as total'),
+                \DB::raw('SUM(case when status = 0 then 1 else 0 end) as available')
+            )
+            ->groupBy('itemcategory_id')
+            ->get();
+        $item = Item::all()->count();
         $user = User::whereHas('roles', function ($query) {
             $query->where('name', '!=', 'superadmin')
                 ->where('name', '!=', 'manager');
         })->get();
-        return view('manager.index', compact('user'));
+        return view('manager.index', compact('user', 'itemsByCategory', 'item'));
     }
 }
--- resources/views/manager/index.blade.php
+++ resources/views/manager/index.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.master')
 @section('content')
+    <h3>List Stok Item</h3>
+    <hr>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
+        <div class="col">
+            <div class="card radius-10 border-start border-0 border-4 border-warning">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <p class="mb-0 text-secondary">Total Items</p>
+                            <h4 class="my-1 text-warning">{{$item}}</h4>
+                            <p class="mb-0 font-13">All Items</p>
+                        </div>
+                        <div class="widgets-icons-2 rounded-circle bg-gradient-orange text-white ms-auto"><i
+                                class='bx bxs-box'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @foreach ($itemsByCategory as $item)
+            <div class="col">
+                <div class="card radius-10 border-start border-0 border-4 border-warning">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div>
+                                <p class="mb-0 text-secondary">{{$item->cat->name}}</p>
+                                <h4 class="my-1 text-warning">{{ $item->total }}</h4>
+                            </div>
+                            <div class="widgets-icons-2 rounded-circle bg-gradient-orange text-white ms-auto"><i
+                                    class='bx bx-box'></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <hr>
     <div class="card">
         <div class="card-head">
             <div class="row">
