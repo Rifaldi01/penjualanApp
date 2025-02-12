37cbb550f681d7289ac3be8574a8916f637d7a1c
Rifaldi01 <rifaldipratama2004@gmail.com>
2025-02-12 16:42:40 +0700

    penambahan fitur

--- resources/views/gudang/permintaanitem/create.blade.php
+++ resources/views/gudang/permintaanitem/create.blade.php
@@ -131,7 +131,7 @@
         // Fungsi untuk mendapatkan opsi aksesori dalam bentuk HTML
         function getAccessoriesOptions() {
             return accessoriesData.map(item => {
-                return `<option value="${item.id}">${item.name} - ${formatRupiah(item.price)}</option>`;
+                return `<option value="${item.id}">${item.name} - ${formatRupiah(item.no_seri)}</option>`;
             }).join('');
         }
 
