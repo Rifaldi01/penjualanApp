f174f2ad2c6e0a22ce48bf7036c8b7d2468f6c51
MacBook Pro <macbookpro@RifaldiSatya.local>
2025-03-03 15:08:21 +0700

    update

--- app/Models/Divisi.php
+++ app/Models/Divisi.php
@@ -10,4 +10,9 @@ class Divisi extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function accessories()
+    {
+        return $this->hasMany(Accessories::class, 'divisi_id', 'id');
+    }
 }
