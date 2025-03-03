42f9bf969c96647af6d0150b0f1802dc0fc24f72
MacBook Pro <macbookpro@RifaldiSatya.local>
2025-03-03 12:04:23 +0700

    update

--- resources/views/gudang/index.blade.php
+++ resources/views/gudang/index.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.master')
-@section('content')
+@section('content')@if(Auth::user()->divisi_id == 6)
+    <h3>List Stok Item</h3>
+    <hr>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
+        <div class="col">
+            <div class="card radius-10 border-start border-0 border-4 border-warning">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <p class="mb-0 text-secondary">Total Items</p>
+                            <h4 class="my-1 text-warning">{{$item}}</h4>
+                            <p class="mb-0 font-13">All Items</p>
+                        </div>
+                        <div class="widgets-icons-2 rounded-circle bg-gradient-orange text-white ms-auto"><i
+                                class='bx bxs-box'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @foreach ($itemsByCategory as $item)
+            <div class="col">
+                <div class="card radius-10 border-start border-0 border-4 border-warning">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div>
+                                <p class="mb-0 text-secondary">{{$item->cat->name}}</p>
+                                <h4 class="my-1 text-warning">{{ $item->total }}</h4>
+                            </div>
+                            <div class="widgets-icons-2 rounded-circle bg-gradient-orange text-white ms-auto"><i
+                                    class='bx bx-box'></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@else
     <h3>List Stok Item</h3>
     <hr>
     <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
@@ -95,6 +133,7 @@
            </div>
        </div>
    </div>
+@endif
 @endsection
 
 @push('head')
