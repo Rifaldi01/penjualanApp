2b6e1bf0f546acd91551d84663dd5ff72e0ce98c
Rifaldi01 <rifaldipratama2004@gmail.com>
2025-11-04 10:06:20 +0700

    report

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -108,7 +108,7 @@ class ReportController extends Controller
             $totalprice += $sale->total_price;
 
             $accessoryCapital = $sale->accessories->sum(function ($accessory) {
-                return $accessory->pivot->subtotal;
+                return $accessory->pivot->qty * $accessory->capital_price;
             });
 
             $itemCapital = $sale->itemSales->sum('capital_price');
