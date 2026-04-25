31b770aed4b31bd888ff0f3506112f0c028adf29
Rifaldi01 <rifaldipratama2004@gmail.com>
2026-04-26 03:33:36 +0700

    konfigurasi sitem

--- resources/views/gudang/permintaanitem/konfirmasi.blade.php
+++ resources/views/gudang/permintaanitem/konfirmasi.blade.php
@@ -26,12 +26,14 @@
                            <td>{{$key +1}}</td>
                            <td>{{ tanggal($permintaan->created_at) }}</td>
                            <td>{{ $permintaan->kode }}</td>
-                           @foreach($permintaan->detailItem as $item)
-                               <li>{{ $item->itemIn->name ?? '-' }}</li>
-                           @endforeach
                            <td>
                                @foreach($permintaan->detailItem as $item)
-                                   <li>{{ $item->itemIn->no_seri ?? '-'}}</li>
+                                   <li>{{ $item->itemIn->name }}</li>
+                               @endforeach
+                           </td>
+                           <td>
+                               @foreach($permintaan->detailItem as $item)
+                                   <li>{{ $item->itemIn->no_seri }}</li>
                                @endforeach
                            </td>
                            <td>{{ $permintaan->jumlah }}</td>
